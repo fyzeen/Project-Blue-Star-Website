323a2aec3eec5fff08e0421c40bdbc65dbc88aff
Fyzeen Ahmad <afyzeen692@gmail.com>
2021-06-15 19:11:06 -0500

    Finished layout_template w/animations and footer

--- flasksite.py
+++ flasksite.py
@@ -5,29 +5,27 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("layout_template.html")
+    return render_template("/pages/home.html", title="Home")
 
 
-@app.route("/about-us")
 @app.route("/about")
 def about_us():
-    return render_template("/pages/about_us.html")
+    return render_template("/pages/about_us.html", title="About Us")
 
 
 @app.route("/our-mission")
 def our_mission():
-    return render_template("/pages/our_mission.html")
+    return render_template("/pages/our_mission.html", title="Our Mission")
 
 
 @app.route("/gallery")
 def gallery():
-    return render_template("/pages/gallery.html")
+    return render_template("/pages/gallery.html", title="Gallery")
 
 
-@app.route("/contact-us")
 @app.route("/contact")
 def contact_us():
-    return render_template("/pages/contact_us.html")
+    return render_template("/pages/contact_us.html", title="Contact Us")
 
 
 if __name__ == '__main__':
