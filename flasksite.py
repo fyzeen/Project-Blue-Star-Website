0a0afa3ca146bdb6c5feae290bed44835dce99b8
Fyzeen Ahmad <afyzeen692@gmail.com>
2021-06-20 18:49:58 -0500

    change flasksite.py for new templates in templates file

--- flasksite.py
+++ flasksite.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('/pages/home.html', title="Home")
+    return render_template('home.html', title="Home")
 
 
 @app.route("/about")
 def about_us():
-    return render_template('/pages/about_us.html', title="About Us")
+    return render_template('about_us.html', title="About Us")
 
 
 @app.route("/our-mission")
 def our_mission():
-    return render_template('/pages/our_mission.html', title="Our Mission")
+    return render_template('our_mission.html', title="Our Mission")
 
 
 @app.route("/gallery")
 def gallery():
-    return render_template('/pages/gallery.html', title="Gallery")
+    return render_template('gallery.html', title="Gallery")
 
 
 @app.route("/contact")
 def contact_us():
-    return render_template('/pages/contact_us.html', title="Contact Us")
+    return render_template('contact_us.html', title="Contact Us")
 
 
 if __name__ == '__main__':
